ef599fec719572eec98abe3f0e24a102f10467e2
m11kmn <mol-am@yandex.ru>
2023-11-03 18:59:49 +0300

    No_Reload_Changes

--- src/main/resources/static/admin.js
+++ src/main/resources/static/admin.js
@@ -1,84 +1,58 @@
-(async function showAllUsers() {
+async function getAllUsers() {
     let response = await fetch('http://localhost:8080/admin/js');
-    let users = await response.json();
-    $('#this-email').text(users[0].username)
-    $('#this-roles').text(users[0].rolesId.map(x => x.replace('ROLE_', '')).toString().replace(',', ' '))
+    return await response.json();
+}
 
-    for (let user of users) {
-        let newRow = '<tr>';
-        let keys = Object.keys(user);
-        for (let key of keys) {
-            if (key !== 'password') {
-                if (key === 'rolesId') {
-                    newRow += ('<td>' + user[key].map(x => x.replace('ROLE_', '')) + '</td>').replace(',', ' ');
-                } else {
-                    newRow += '<td>' + user[key] + '</td>';
-                }
-            }
-        }
-        newRow += '<td><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#edit-modal-dialog" name="edit-button" id="'+user['id']+'">Edit</button></td>';
-        newRow += '<td><button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#delete-modal-dialog" name="delete-button" id="'+user['id']+'">Delete</button></td>';
-        $('tbody').append(newRow);
-    }
+let editUser = {}
+let deleteUser = {}
 
-    $('button[name="edit-button"], button[name="delete-button"]').on('click', function() {
+function showAllUsers() {
+    $('tbody tr').remove();
+    getAllUsers().then(users => {
+        $('#this-email').text(users[0].username)
+        $('#this-roles').text(users[0].rolesId.map(x => x.replace('ROLE_', '')).toString().replace(',', ' '))
         for (let user of users) {
-            if (user.id === parseInt(this.id)) {
-                let keys = Object.keys(user);
-
-                let editUser = {
-                }
-                let deletedUser = {
-                }
-
-                for (let key of keys) {
-                    if (key !== 'password') {
-                        $('#'+key+'1').val(user[key])
-                        editUser[key] = user[key]
-
-                        $('#'+key+'2').val(user[key])
-                        deletedUser[key] = user[key]
+            let newRow = '<tr id="tr-'+user['id']+'">';
+            let keys = Object.keys(user);
+            for (let key of keys) {
+                if (key !== 'password') {
+                    if (key === 'rolesId') {
+                        newRow += ('<td>' + user[key].map(x => x.replace('ROLE_', '')) + '</td>').replace(',', ' ');
+                    } else {
+                        newRow += '<td>' + user[key] + '</td>';
                     }
                 }
-
-                $('#firstName1').change(function () {editUser.firstName = $('#firstName1').val()})
-                $('#lastName1').change(function () {editUser.lastName = $('#lastName1').val()})
-                $('#age1').change(function () {editUser.age = $('#age1').val()})
-                $('#username1').change(function () {editUser.username = $('#username1').val()})
-                $('#password1').change(function () {editUser.password = $('#password1').val()})
-                $('#rolesId1').change(function () {editUser.rolesId = $('#rolesId1').val()})
-
-                $('#edit-user').on('click', async function (){
-                    const headers = {
-                        'Content-Type': 'application/json'
-                    }
-
-                    let response = await fetch('http://localhost:8080/admin/js', {
-                        method: 'PUT',
-                        body: JSON.stringify(editUser),
-                        headers: headers
-                    });
-                })
-
-                $('#delete-user').on('click', async function (){
-                    const headers = {
-                        'Content-Type': 'application/json'
-                    }
-
-                    let response = await fetch('http://localhost:8080/admin/js', {
-                        method: 'DELETE',
-                        body: JSON.stringify(deletedUser),
-                        headers: headers
-                    });
-                })
-
             }
+            newRow += '<td><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#edit-modal-dialog" name="edit-button" id="'+user['id']+'">Edit</button></td>';
+            newRow += '<td><button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#delete-modal-dialog" name="delete-button" id="'+user['id']+'">Delete</button></td>';
+            $('tbody').append(newRow);
         }
+        $('button[name="edit-button"], button[name="delete-button"]').on('click', function() {
+            console.log(editUser)
+            console.log(deleteUser)
+            getAllUsers().then(users => {
+                for (let user of users) {
+                    if (user.id === parseInt(this.id)) {
+                        let keys = Object.keys(user);
+                        for (let key of keys) {
+                            if (key !== 'password') {
+                                $('#' + key + '1').val(user[key])
+                                editUser[key] = user[key]
+
+                                $('#' + key + '2').val(user[key])
+                                deleteUser[key] = user[key]
+                            }
+                        }
+                    }
+                }
+            })
+        })
     })
-})();
+}
+showAllUsers()
+
 
 ////////////////////////        Создание и отправка нового чела         ///////////////////////////////
-
 let newUserForm = $('#new-user-form')
 let newUser = {}
 newUserForm.change(function () {
@@ -88,8 +62,8 @@ newUserForm.change(function () {
     newUser.username = newUserForm.find('input[name="email"]').val();
     newUser.password = newUserForm.find('input[name="password"]').val();
     newUser.rolesId = newUserForm.find('select[name="rolesId"]').val();
+    console.log(newUser);
 })
-
 $('#add-new-user').on('click', async function (){
     const headers = {
         'Content-Type': 'application/json'
@@ -99,5 +73,44 @@ $('#add-new-user').on('click', async function (){
         body: JSON.stringify(newUser),
         headers: headers
     });
-    console.log(newUser)
+    $('#v-pills-home-tab').click();
+    showAllUsers();
+})
+
+
+////////////////////////        Редактировнаие и отправка чела         ///////////////////////////////
+let editUserForm = $('#edit-user-form')
+editUserForm.change(function () {
+    editUser.firstName = editUserForm.find('input[name="firstname"]').val();
+    editUser.lastName = editUserForm.find('input[name="lastname"]').val();
+    editUser.age = editUserForm.find('input[name="age"]').val();
+    editUser.username = editUserForm.find('input[name="email"]').val();
+    editUser.password = editUserForm.find('input[name="password"]').val();
+    editUser.rolesId = editUserForm.find('select[name="rolesId"]').val();
+    console.log(editUser);
+})
+$('#edit-user').on('click', async function (){
+    const headers = {
+        'Content-Type': 'application/json'
+    }
+    let response = await fetch('http://localhost:8080/admin/js', {
+        method: 'PUT',
+        body: JSON.stringify(editUser),
+        headers: headers
+    });
+    $('#v-pills-home-tab').click();
+    showAllUsers();
+})
+
+////////////////////////        Удаление чела         ///////////////////////////////
+$('#delete-user').on('click', async function (){
+    const headers = {
+        'Content-Type': 'application/json'
+    }
+    let response = await fetch('http://localhost:8080/admin/js', {
+        method: 'DELETE',
+        body: JSON.stringify(deleteUser),
+        headers: headers
+    });
+    showAllUsers();
 })
